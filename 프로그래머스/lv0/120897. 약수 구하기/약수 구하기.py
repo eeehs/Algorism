7ec1d163dafaba3a2750f0951f226a99eb693c9f
eeehs <107851776+eeehs@users.noreply.github.com>
2022-12-08 16:07:03 +0900

    [level 0] Title: 약수 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.py
@@ -1,6 +1,6 @@
 def solution(n):
     answer = []
     for i in range(1, n+1):
-        if (n % i == 0) and (i not in answer):
+        if (n % i == 0):
             answer.append(i)
     return answer
